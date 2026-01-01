781e7d9f93493ccaed6054c8c33c19e2e2177711
MARohrdanz <marohrdanz@mdanderson.org>
2026-01-01 10:51:15 -0600

    Remove log file generation
    This repo is just for poking around and learning things. No need to
    write a log file.

--- app/log_setup.py
+++ app/log_setup.py
@@ -25,15 +25,6 @@ def configure_logging(level='INFO'):
     stream_handler.setFormatter(formatter)
     logger= logging.getLogger(__name__)
     logger.addHandler(stream_handler)
-    # File handler for file output
-    file_handler = logging.FileHandler('app.log')
-    file_formatter = ColoredFormatter(
-      '%(asctime)s - %(levelname)s - %(message)s',
-      datefmt='%Y-%m-%d %H:%M:%S',
-      reset=True
-    )
-    file_handler.setFormatter(file_formatter)
-    logger.addHandler(file_handler)
     logger.setLevel(level)
   return logger
 
